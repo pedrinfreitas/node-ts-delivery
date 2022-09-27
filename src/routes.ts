1aba0adcd1729dfb2bbd4ef9191b9772da0f1792
Pedro Freitas <pedrinfreitas@gmail.com>
2022-09-27 19:25:58 -0300

    feat: auth deliveryman + token

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,20 @@
 import { Router } from 'express';
 
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
+import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateClientController';
 import { CreateClienteController } from './modules/clients/useCases/createClient/CreateClientController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 
 const routes = Router();
 
 const authenticateClientController = new AuthenticateClientController();
+const authenticateDeliverymanController =
+  new AuthenticateDeliverymanController();
 const createClientController = new CreateClienteController();
 const createDeliverymanController = new CreateDeliverymanController();
 
-routes.post("/auth/", authenticateClientController.handle);
+routes.post("/auth/client", authenticateClientController.handle);
+routes.post("/auth/deliveryman", authenticateDeliverymanController.handle);
 routes.post("/client/", createClientController.handle);
 routes.post("/deliveryman/", createDeliverymanController.handle);
 
